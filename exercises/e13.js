1d994a43cc1490e23e994827189492686ae00de4
Miguel Mendoza <miguel.mendoza8602@gmail.com>
2024-09-26 12:16:22 -0500

    solved e13

--- exercises/e13.js
+++ exercises/e13.js
@@ -9,7 +9,26 @@
 // Phil's Account Object
 // ]
 
-export function getAllAccountsWithSumsOfDepositsLess2000(array) {}
+import { bankAccounts } from "../data/data";
+
+export function getAllAccountsWithSumsOfDepositsLess2000(array) {
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let sum = 0;
+    // if (array[i].deposits === undefined) {
+    //   newArr.push(array[i]);
+    // }
+    for (let x in array[i].deposits) {
+      sum += array[i].deposits[x];
+    }
+    console.log(sum);
+    if (sum < 2000) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
