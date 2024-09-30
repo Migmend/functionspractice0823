55a34ec8142c19639088372181c1b0c16549a508
Miguel Mendoza <miguel.mendoza8602@gmail.com>
2024-09-30 17:29:34 -0500

    solved e11

--- exercises/e11.js
+++ exercises/e11.js
@@ -4,9 +4,38 @@
 // Array example: bankAccounts in /data/data.js
 // getAllWithdrawals(bankAccounts) => [3432, 0, 43242.34, 0, 23432]
 
+import { bankAccounts } from "../data/data";
+
 export function getAllWithdrawals(array) {
   // Your code goes here...
+  let total = [];
 
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i].withdrawals;
+    let sum = 0;
+    if (num && num.length > 0) {
+      for (let x = 0; x < num.length; x++) {
+        sum += num[x];
+      }
+    } else {
+      sum = 0;
+    }
+
+    total.push(sum);
+  }
+  return total;
+
+  // let withdrawalsSum = 0;
+  // let newArr = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i].withdrawls) {
+  //     for (let w = 0; w < array.length; w++) {
+  //       withdrawalsSum += array[w].withdrawls[w];
+  //       newArr.push(withdrawalsSum);
+  //     }
+  //   }
+  // }
+  // return newArr;
 }
 
 // === TEST YOURSELF ===
