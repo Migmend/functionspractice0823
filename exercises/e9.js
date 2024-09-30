4082204d350038e24e603681e30a1bd0e81b6c13
Miguel Mendoza <miguel.mendoza8602@gmail.com>
2024-09-30 17:19:26 -0500

    solved e9

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,15 +1,21 @@
-
 // EXERCISE 9
 // Return an array of bank accounts with balance over $100
 // Array example: bankAccounts in /data/data.js
 // getClientsWithBalanceOverOneHundred(array) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
+import { bankAccounts } from "../data/data";
+
 export function getClientsWithBalanceOverOneHundred(array) {
   // Your code goes here...
+  const overOneHundered = [];
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].balance > 100) {
+      overOneHundered.push(array[i]);
+    }
+  }
+  return overOneHundered;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
