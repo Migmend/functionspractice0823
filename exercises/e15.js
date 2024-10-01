77f5ea38f7fdb3374e96581977e46bac46559c6f
Miguel Mendoza <miguel.mendoza8602@gmail.com>
2024-10-01 18:17:35 -0500

    solved e15

--- exercises/e15.js
+++ exercises/e15.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 15
 // Please, read the exercise-info/concat.md to get the initial data of what is the expected result of this exercise.
 // concatArrays(['d', 'r', 'a'], ['f', 'y']) => ['d', 'r', 'a', 'f', 'y']
@@ -6,10 +5,18 @@
 
 export function concatArrays(arr1, arr2) {
   // Your code goes here...
+  let total = [];
 
+  for (let i = 0; i < arr1.length; i++) {
+    total.push(arr1[i]);
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    total.push(arr2[i]);
+  }
+
+  return total;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-15"
